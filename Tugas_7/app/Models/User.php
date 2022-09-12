b812da31141c90ba9ece84926b9e622a822e2612
darmawan02 <107595676+darmawan02@users.noreply.github.com>
2022-09-12 20:14:36 +0700

    Create Tugas7

---
+++ Tugas_7/app/Models/User.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+use App\Models\UserDetail;
+class User extends Authenticatable
+{   
+    protected $table = 'user';
+    use HasApiTokens, HasFactory, Notifiable;
+
+    function detail(){
+        return $this->hasOne(UserDetail::class, 'id_user');
+    }
+
+    function produk(){
+        return $this->hasMany(produk::class, 'id_user');
+    }
+
+    // /**
+    //  * The attributes that are mass assignable.
+    //  *
+    //  * @var array<int, string>
+    //  */
+    // protected $fillable = [
+    //     'name',
+    //     'email',
+    //     'password',
+    // ];
+
+    // /**
+    //  * The attributes that should be hidden for serialization.
+    //  *
+    //  * @var array<int, string>
+    //  */
+    // protected $hidden = [
+    //     'password',
+    //     'remember_token',
+    // ];
+
+    // /**
+    //  * The attributes that should be cast.
+    //  *
+    //  * @var array<string, string>
+    //  */
+    // protected $casts = [
+    //     'email_verified_at' => 'datetime',
+    // ];
+}
---
+++ Tugas_7/app/Models/produk.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\models;
+
+class Produk extends Model{
+    protected $table = 'produk';
+
+    function seller(){
+        return $this->belongsTo(User::class, 'id_user');
+    }
+}
